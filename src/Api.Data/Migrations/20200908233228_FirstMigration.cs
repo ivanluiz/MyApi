ab43630f6b4be35fbf1ee8c917d244ae8229af86
ivanluiz <ivanluizg@gmail.com>
2020-09-08 21:06:25 -0300

    Teste de migrations

---
+++ src/Api.Data/Migrations/20200908233228_FirstMigration.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Directory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateAt = table.Column<DateTime>(nullable: true),
+                    UpdateAt = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GeometricForm",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateAt = table.Column<DateTime>(nullable: true),
+                    UpdateAt = table.Column<DateTime>(nullable: false),
+                    DirectoryId = table.Column<Guid>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Type = table.Column<string>(nullable: false),
+                    Color = table.Column<string>(maxLength: 60, nullable: false),
+                    Size = table.Column<int>(maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeometricForm", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GeometricForm_Directory_DirectoryId",
+                        column: x => x.DirectoryId,
+                        principalTable: "Directory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeometricForm_DirectoryId",
+                table: "GeometricForm",
+                column: "DirectoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GeometricForm");
+
+            migrationBuilder.DropTable(
+                name: "Directory");
+        }
+    }
+}
